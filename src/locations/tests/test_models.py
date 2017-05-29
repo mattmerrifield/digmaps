abb978e8eb1608820ce375b26313d6eea91277f8
Matt Merrifield <mattmerrifield@visionfleet.com>
2017-05-29 16:58:02 -0700

    First unit testing is passing

--- src/locations/tests/test_models.py
+++ src/locations/tests/test_models.py
@@ -11,15 +11,12 @@ class RegionTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.parent = RegionFactory()
-
         cls.child1 = RegionFactory(parent=cls.parent)
         cls.child2 = RegionFactory(parent=cls.parent)
-
         cls.grand1a = RegionFactory(parent=cls.child1)
         cls.grand1b = RegionFactory(parent=cls.child1)
         cls.grand2a = RegionFactory(parent=cls.child2)
         cls.grand2b = RegionFactory(parent=cls.child2)
-
         cls.great1a_1 = RegionFactory(parent=cls.grand1a)
         cls.great1a_2 = RegionFactory(parent=cls.grand1a)
 
