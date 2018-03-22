5137793d2a2c0f2b2f2e2aced1fbc880d90d8abd
Matt Merrifield <mattmerrifield@visionfleet.com>
2018-03-21 22:51:22 -0700

    Preliminary API design tests

---
+++ src/locations/tests/test_api.py
@@ -0,0 +1,69 @@
+from django import test
+from rest_framework import test
+
+from locations import models
+from locations.tests import factories
+
+
+
+class APITestCase(test.APITestCase):
+    """
+    Instructive examples about how to use the API
+    """
+
+
+class RegionEndpointTests(APITestCase):
+    """
+    The region endpoint supports the standard actions.
+    """
+    def setUpTestData(cls):
+        cls.region_1 = factories.RegionFactory()
+        cls.region_2 = factories.RegionFactory()
+
+    def setUp(self):
+        super(RegionEndpointTests, self).setUp()
+        self.assertEqual(2, models.Region.objects.count())
+
+    def test_list_regions(self):
+        """
+        A plain GET To the regions endpoint returns a paginated list of regions
+        """
+        regions = self.client.get('/regions/')
+        print(regions)
+
+    def test_get_region(self):
+        """
+        You may request a specific region by its ID
+        """
+        region = self.client.get('/regions/{}'.format(self.region_1.id))
+        print(region)
+
+    def test_create_region(self):
+        """
+        You may POST some JSON to create a new region
+        """
+        response = self.client.post('/regions/', data={
+            'name': 'New Region',
+            'description': 'A land of new opportunities.',
+        })
+        print(response)
+        self.assertEqual(3, models.Region.objects.count())
+
+    def test_delete_region(self):
+        """
+        You may delete a region. Doing so will remove it from the database, but will NOT delete the associated sites.
+        """
+        response = self.client.delete('/regions/{}'.format(self.region_2.id))
+        print(response)
+        self.assertEqual(1, models.Region.objects.count())
+
+    def test_partial_update_region(self):
+        old_text = self.region_2.description
+        response = self.client.patch('/regions/{}'.format(self.region_2.id), data={
+            'description': 'We have updated the description field here',
+        })
+        print(response)
+        self.region_2.refresh_from_db()
+        new_text = self.region_2.description
+        self.assertNotEquals(old_text, new_text)
+        self.assertEqual(new_text, 'We have updated the description field here')
