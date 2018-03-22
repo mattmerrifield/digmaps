45e63a65d1ba6c67ae245ddf206343f22c15eefe
Matt Merrifield <mattmerrifield@visionfleet.com>
2018-03-21 22:39:35 -0700

    Some factories. No tests.

--- src/locations/tests/factories.py
+++ src/locations/tests/factories.py
@@ -1,7 +1,24 @@
 from factory import DjangoModelFactory
-from locations.models import Region
+from locations import models
 
 
 class RegionFactory(DjangoModelFactory):
     class Meta:
-        model = Region
+        model = models.Region
+
+
+class SiteFactory(DjangoModelFactory):
+    region = RegionFactory
+
+    class Meta:
+        model = models.Site
+
+
+class FeatureFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Feature
+
+
+class PeriodFactory(DjangoModelFactory):
+    class Meta:
+        model = models.Period
