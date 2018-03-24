377ba07adacc4cd7583cab21b960f5fea5fe7748
Matt Merrifield <mattmerrifield@visionfleet.com>
2018-03-23 20:09:25 -0700

    flatten out django files

--- api/settings.py
+++ api/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'digmaps.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'digmaps.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
