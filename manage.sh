315ace657e409afaa5d72be9913559ffa3eb7c63
Matt Merrifield <mattmerrifield@visionfleet.com>
2018-03-23 20:23:52 -0700

    permissions and stuff

--- manage.sh
+++ manage.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 COMPOSE_FILE="$DIR/docker-compose.yml"
-docker-compose -f COMPOSE_FILE run api ./manage.py $@
+docker-compose -f $COMPOSE_FILE exec api ./manage.py $@
