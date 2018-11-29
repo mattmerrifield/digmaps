a5dbc51d87a7b5e41e87a647710465250e48e641
matt <rmatthewmerrifield@gmail.com>
2018-11-28 23:01:53 -0800

    Something resembling a working graphql endpoint

--- api/gql/schema.py
+++ api/gql/schema.py
@@ -1,8 +1,31 @@
 from graphene_django import DjangoObjectType
+from graphene_django.converter import convert_django_field
 import graphene
-import graphql_geojson
 from locations import models as locations
 from bibliography import models as bibliography
+from django.contrib.gis.db.models import PointField
+
+
+class PointType(graphene.ObjectType):
+    """
+    Very simple GeoJSON representation for a PointField
+    """
+    type = graphene.String()
+    coordinates = graphene.List(graphene.Float)
+
+    def resolve_type(self, info):
+        """
+        A point is.... always a point. Duh.
+        """
+        return "point"
+
+    def resolve_coordinates(self, info):
+        return info.x, info.y
+
+
+@convert_django_field.register(PointField)
+def convert_point_field(field, registry=None):
+    return PointType()
 
 
 class Region(DjangoObjectType):
@@ -10,10 +33,9 @@ class Region(DjangoObjectType):
         model = locations.Region
 
 
-class Site(graphql_geojson.GeoJSONType):
+class Site(DjangoObjectType):
     class Meta:
         model = locations.Site
-        geojson_field = 'coordinates'
 
 
 class Feature(DjangoObjectType):
@@ -33,10 +55,21 @@ class SiteFeature(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     regions = graphene.List(Region)
-    sites = graphene.List(Site)
+    sites = graphene.List(Site, id=graphene.ID())
+    features = graphene.List(Feature)
+    periods = graphene.List(Period)
 
-    def resolve_users(self, info):
+    def resolve_regions(self, info):
         return locations.Region.objects.all()
+
+    def resolve_sites(self, info, id):
+        return locations.Site.objects.filter(id=id)
+
+    def resolve_features(self, info):
+        return locations.Feature.objects.all()
+
+    def resolve_periods(self, info):
+        return locations.Period.objects.all()
 
 
 schema = graphene.Schema(query=Query)
