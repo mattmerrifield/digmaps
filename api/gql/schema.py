156b46171c8d54d8c0fa3d589c2a70e1ed29b8c8
matt <rmatthewmerrifield@gmail.com>
2019-05-04 13:20:46 -0700

    schema tweaks

--- api/gql/schema.py
+++ api/gql/schema.py
@@ -46,14 +46,17 @@ class Query(graphene.ObjectType):
     features = graphene.List(Feature)
     periods = graphene.List(Period)
 
-    # def resolve_regions(self, info):
-    #     return locations.Region.objects.all()
-    #
-    # def resolve_features(self, info):
-    #     return locations.Feature.objects.all()
-    #
-    # def resolve_periods(self, info):
-    #     return locations.Period.objects.all()
+    def resolve_sites(self, info, id, modern_name, ancient_name):
+        return locations.Site.objects.all()
+
+    def resolve_regions(self, info):
+        return locations.Region.objects.all()
+
+    def resolve_features(self, info):
+        return locations.Feature.objects.all()
+
+    def resolve_periods(self, info):
+        return locations.Period.objects.all()
 
 
 schema = graphene.Schema(query=Query)
