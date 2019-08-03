b2f10e7dfb732b22aad2986aba604622c18572ab
matt <rmatthewmerrifield@gmail.com>
2019-08-03 11:52:50 -0700

    basic layout experiments. Websocket support. Dotenv library

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,13 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Box} from "rebass";
+import {Box, Flex, Text} from "rebass";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-        <Box>Hello World</Box>
-    </div>
+    <Flex
+        className="App"
+    >
+        <Flex
+            bg={'black'}
+            flexDirection={'column'}
+        >
+            <Flex
+                bg={'orange'}
+                flexDirection={'row'}
+                justifyContent={'center'}
+            >
+                <Text>Header</Text>
+            </Flex>
+
+            <Flex
+                flexDirection={'row'}
+            >
+                <Box
+                    width={1/5}
+                    bg={'blue'}
+                >
+                    <Text>Left</Text>
+                </Box>
+                <Box width={3/5} bg={'white'}>
+                    <Text>Center</Text>
+                </Box>
+                <Box width={1/5} bg={'red'}>
+                    <Text>Right</Text>
+                </Box>
+            </Flex>
+
+            <Flex
+                bg={'green'}
+                flexDirection={'row'}
+                justifyContent={'center'}
+            >
+                <Text>
+                    Footer
+                </Text>
+            </Flex>
+        </Flex>
+    </Flex>
   );
 };
 
